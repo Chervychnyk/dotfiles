dc661afe2b9cf9709c2b76aa5184e47902a6bd33
Artem Chervychnyk <artem@themindstudios.com>
2026-09-15 09:43:49 +0300

    style: align active themes with Everforest Dark
    Use the same Everforest Dark Hard palette across the prompt and macOS status bar to remove the remaining Kanagawa colors.

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-# Kanagawa Wave Color Palette
-export BLACK=0xff1f1f28
-export WHITE=0xffdcd7ba
-export RED=0xffc34043
-export GREEN=0xff76946a
-export BLUE=0xff7e9cd8
-export YELLOW=0xffe6c384
-export ORANGE=0xffffa066
-export MAGENTA=0xff957fb8
-export MAUVE=0xff957fb8
-export PINK=0xff938aa9
-export TEAL=0xff6a9589
-export SKY=0xff7aa89f
-export LAVENDER=0xff7fb4ca
-export GREY=0xff727169
-export SURFACE0=0xff2a2a37
-export SURFACE1=0xff363646
-export SURFACE2=0xff54546d
-export OVERLAY0=0xff727169
+# Everforest Dark Hard Color Palette
+export BLACK=0xff1e2326
+export WHITE=0xffd3c6aa
+export RED=0xffe67e80
+export GREEN=0xffa7c080
+export BLUE=0xff7fbbb3
+export YELLOW=0xffdbbc7f
+export ORANGE=0xffe69875
+export MAGENTA=0xffd699b6
+export MAUVE=0xffd699b6
+export PINK=0xffd699b6
+export TEAL=0xff83c092
+export SKY=0xff83c092
+export LAVENDER=0xff7fbbb3
+export GREY=0xff7a8478
+export SURFACE0=0xff2e383c
+export SURFACE1=0xff374145
+export SURFACE2=0xff414b50
+export OVERLAY0=0xff859289
 export TRANSPARENT=0x00000000
 
 # General bar colors
-export BAR_COLOR=0xcc1f1f28
+export BAR_COLOR=0xcc272e33
 export ICON_COLOR=$WHITE # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0xc02a2a37
-export BACKGROUND_2=0xc0363646
+export BACKGROUND_1=0xc02e383c
+export BACKGROUND_2=0xc0374145
 
-export POPUP_BACKGROUND_COLOR=0xff1f1f28
+export POPUP_BACKGROUND_COLOR=0xff272e33
 export POPUP_BORDER_COLOR=$BLUE
 
 export SHADOW_COLOR=$BLACK
